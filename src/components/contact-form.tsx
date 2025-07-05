1a571a9bd7cbe00fc29dbd4b48d012dc2e74eaae
jackvdp <jack@pumpymusic.co.uk>
2025-07-05 19:48:31 +0100

    Make contact form background lighter

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -30,7 +30,7 @@ export default function ContactForm() {
   }
 
   return (
-    <section className="py-16 sm:py-24 lg:py-32 bg-liberty-secondary relative overflow-hidden">
+    <section className="py-16 sm:py-24 lg:py-32 bg-liberty-secondary/40 relative overflow-hidden">
 
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Section Header */}
