da1634ed101a1e490f934354f464765ed1c76aaf
jackvdp <jack@pumpymusic.co.uk>
2025-07-06 11:15:51 +0100

    Increase faq animation effect

--- src/components/faq-section.tsx
+++ src/components/faq-section.tsx
@@ -176,10 +176,10 @@ export default function FAQSection() {
           <AnimatePresence mode="wait">
             <motion.div
               key={activeCategory}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.2 }}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.3 }}
               className="space-y-4"
             >
               {categoryFaqs.map((faq, index) => (
