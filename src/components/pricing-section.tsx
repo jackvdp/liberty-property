8264e2f542b46b9a5452dd20ebaa2d8eacf34524
jackvdp <jack@pumpymusic.co.uk>
2025-08-04 20:48:05 +0100

    fix build errors

--- src/components/pricing-section.tsx
+++ src/components/pricing-section.tsx
@@ -6,8 +6,7 @@ import { motion } from 'framer-motion'
 import { 
   PoundSterling, 
   Shield, 
-  TrendingDown, 
-  ArrowRight,
+  TrendingDown,
   CheckCircle,
   MessageCircle,
   Calculator,
--- src/components/who-we-are.tsx
+++ src/components/who-we-are.tsx
@@ -8,7 +8,6 @@ import {
   Shield, 
   TrendingDown, 
   ArrowRight,
-  ExternalLink,
   CheckCircle,
   Heart,
   MessageCircle
