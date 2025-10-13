0605d9057a15765b9181029cf4e633210c48e6ef
jackvdp <jack@pumpymusic.co.uk>
2025-10-13 09:49:35 +0100

    Fix sidebar links

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -10,6 +10,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import * as React from "react";
 
 export function NavMain({
   items,
@@ -26,9 +27,11 @@ export function NavMain({
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title}>
-                {item.icon && <item.icon />}
-                <span>{item.title}</span>
+              <SidebarMenuButton asChild>
+                <a href={item.url}>
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                </a>
               </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
